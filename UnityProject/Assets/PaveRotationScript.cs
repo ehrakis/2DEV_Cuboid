a36a5336e809d40e8b4afce93cb3b9e985c4354b
ehrakis <clement.grimaud@supinfo.com>
2018-03-23 11:12:18 +0100

    Implement mouvement counter

--- UnityProject/Assets/PaveRotationScript.cs
+++ UnityProject/Assets/PaveRotationScript.cs
@@ -6,27 +6,43 @@ public class PaveRotationScript : MonoBehaviour {
 
     public Transform parent;
 
-    private int state { get; set; }
+    private int State = 1;
+    public int DeplacementNumber = 0;
     private float xSize = 0;
     private float zSize = 0;
+
+    public int GetState()
+    {
+        return State;
+    }
+
+    public int GetDeplcementNumber()
+    {
+        return DeplacementNumber;
+    }
+
+    private void IncreaseMouvementNumber()
+    {
+        DeplacementNumber++;
+    }
 
     public void SetState(int value)
     {
         if(value == 1)
         {
-            state = 1;
+            State = 1;
             xSize = 0;
             zSize = 0;
         }
         else if (value == 2)
         {
-            state = 2;
+            State = 2;
             xSize = 0.5f;
             zSize = 0;
         }
         else if (value == 3)
         {
-            state = 3;
+            State = 3;
             xSize = 0;
             zSize = 0.5f;
         }
@@ -34,15 +50,15 @@ public class PaveRotationScript : MonoBehaviour {
 
     private void changeState(Vector3 direction)
     {
-        if(state == 1 && (direction == Vector3.right || direction == Vector3.left))
+        if(State == 1 && (direction == Vector3.right || direction == Vector3.left))
         {
             SetState(2);
         }
-        else if(state == 1 && (direction == Vector3.forward || direction == Vector3.back))
+        else if(State == 1 && (direction == Vector3.forward || direction == Vector3.back))
         {
             SetState(3);
         }
-        else if((state == 2 && (direction == Vector3.right || direction == Vector3.left))|| (state == 3 && (direction == Vector3.forward || direction == Vector3.back)))
+        else if((State == 2 && (direction == Vector3.right || direction == Vector3.left))|| (State == 3 && (direction == Vector3.forward || direction == Vector3.back)))
         {
             SetState(1);
         }
@@ -50,7 +66,7 @@ public class PaveRotationScript : MonoBehaviour {
 
     private float ChooseAddValue(Vector3 direction)
     {
-        if (state != 1)
+        if (State != 1)
         {
             if (direction == Vector3.right || direction == Vector3.left)
             {
@@ -78,11 +94,11 @@ public class PaveRotationScript : MonoBehaviour {
         changeState(direction);
         addValue = ChooseAddValue(direction);
         parent.Translate(direction * (addValue + 0.5f));
+        IncreaseMouvementNumber();
     }
 
     // Use this for initialization
     void Start () {
-        SetState(1);
 	}
 	
 	// Update is called once per frame
