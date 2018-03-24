d95267707662970066a67a57234df0d7bc9cd84b
Clément Grimaud <clement.grimaud@supinfo.com>
2018-03-24 01:44:24 +0100

    Cube now fall when there is no more Tiles under it

--- UnityProject/Assets/Script/PaveRotationScript.cs
+++ UnityProject/Assets/Script/PaveRotationScript.cs
@@ -9,6 +9,10 @@ public class PaveRotationScript : MonoBehaviour {
     private int DeplacementNumber = 0;
     private float xSize = 0;
     private float zSize = 0;
+    private int CollisionNumber = 0;
+    private bool AllowInput = true;
+    private int FrameWithoutContact = 0;
+    private Vector3 LastMouvement;
 
     public int GetState()
     {
@@ -87,12 +91,64 @@ public class PaveRotationScript : MonoBehaviour {
         RotationPoint = new Vector3(transform.position.x, 0, transform.position.z) + (direction * (ChooseAddValue(direction) + 0.5f));
     }
 
+    public void Expulse()
+    {
+        if(++FrameWithoutContact >= 3)
+        {
+            //Rotate(LastMouvement, LastRotation);
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+            if (LastMouvement==Vector3.down)
+                GetComponent<Rigidbody>().AddTorque(Vector3.left*10);
+            else if (LastMouvement == Vector3.up)
+                GetComponent<Rigidbody>().AddTorque(Vector3.right * 10);
+            else if(LastMouvement==Vector3.left)
+                GetComponent<Rigidbody>().AddTorque(Vector3.forward * 10);
+            else if (LastMouvement == Vector3.right)
+                GetComponent<Rigidbody>().AddTorque(Vector3.back * 10);
+        }
+    }
+
+    public void TestStability()
+    {
+        if (GetState() != 1 && CollisionNumber < 2)
+        {
+            AllowInput = false;
+            Expulse();
+        }
+        else if(GetState() != 1 && CollisionNumber == 2)
+        {
+            AllowInput = true;
+            FrameWithoutContact = 0;
+        }
+        else if (GetState() == 1 && CollisionNumber < 1)
+        {
+            AllowInput = false;
+            Expulse();
+        }
+        else if (GetState() == 1 && CollisionNumber == 1)
+        {
+            AllowInput = true;
+            FrameWithoutContact = 0;
+        }
+    }
+
     public void Rotate(Vector3 direction, Vector3 rotation)
     {
+        LastMouvement = direction;
+        CollisionNumber = 0;
         SetRotationPoint(direction);
         transform.RotateAround(RotationPoint, rotation, 90);
         changeState(direction);
         IncreaseMouvementNumber();
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.tag == "Tile")
+        {
+            CollisionNumber++;
+        }
     }
 
     // Use this for initialization
@@ -102,21 +158,26 @@ public class PaveRotationScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && AllowInput)
         {
             Rotate(Vector3.left, new Vector3(0, 0, 1));
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && AllowInput)
         {
             Rotate(Vector3.right, new Vector3(0, 0, -1));
         }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && AllowInput)
         {
             Rotate(Vector3.forward, new Vector3(1, 0, 0));
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && AllowInput)
         {
             Rotate(Vector3.back, new Vector3(-1, 0, 0));
         }
+    }
+
+    private void LateUpdate()
+    {
+        TestStability();
     }
 }
